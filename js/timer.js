12fe8abfce076a8a809b5c1397f25eecc86cc462
Ramanpreet Singh <ramanpreet6262@gmail.com>
2018-08-31 20:18:36 +0530

    timer and demo timeline changes

---
+++ js/timer.js
@@ -0,0 +1,32 @@
+const year = (new Date().getFullYear());
+const fourthOfJuly = new Date(year, 10,5).getTime();
+
+// countdown
+let timer = setInterval(function() {
+
+  // get today's date
+  const today = new Date().getTime();
+
+  // get the difference
+  const diff = fourthOfJuly - today;
+
+  // math
+  let days = Math.floor(diff / ( 1000*60 * 60 * 24));
+  days-=30;
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // display
+  document.getElementById("timer").innerHTML =
+    "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
+}, 1000);
